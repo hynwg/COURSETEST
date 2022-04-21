a0750d7df58e870429e7f26dee72bb93ffbf2bfa
hynwg <13488160731@163.com>
2022-04-21 15:20:51 +0800

    简化登陆页面

--- course/admin/src/App.vue
+++ course/admin/src/App.vue
@@ -1,26 +1,100 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-container">
+    <div class="main-content">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+          <div class="login-container">
+            <div class="center">
+              <h1>
+                <i class="ace-icon fa fa-leaf green"></i>
+                <span class="red">控台登陆</span>
+                <!--                <span class="white" id="id-text2">Application</span>-->
+              </h1>
+              <h4 class="blue" id="id-company-text">&copy; Company Name</h4>
+            </div>
+
+            <div class="space-6"></div>
+
+            <div class="position-relative">
+              <div id="login-box" class="login-box visible widget-box no-border">
+                <div class="widget-body">
+                  <div class="widget-main">
+                    <h4 class="header blue lighter bigger">
+                      <i class="ace-icon fa fa-coffee green"></i>
+                      请输入用户和密码
+                    </h4>
+
+                    <div class="space-6"></div>
+
+                    <form>
+                      <fieldset>
+                        <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="text" class="form-control" placeholder="Username"/>
+															<i class="ace-icon fa fa-user"></i>
+														</span>
+                        </label>
+
+                        <label class="block clearfix">
+														<span class="block input-icon input-icon-right">
+															<input type="password" class="form-control" placeholder="Password"/>
+															<i class="ace-icon fa fa-lock"></i>
+														</span>
+                        </label>
+
+                        <div class="space"></div>
+
+                        <div class="clearfix">
+                          <label class="inline">
+                            <input type="checkbox" class="ace"/>
+                            <span class="lbl"> 记住我</span>
+                          </label>
+
+                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                            <i class="ace-icon fa fa-key"></i>
+                            <span class="bigger-110">登陆</span>
+                          </button>
+                        </div>
+
+                        <div class="space-4"></div>
+                      </fieldset>
+                    </form>
+
+
+                    <div class="toolbar clearfix">
+                      <div>
+                        <a href="#" data-target="#forgot-box" class="forgot-password-link">
+                          <i class="ace-icon fa fa-arrow-left"></i>
+                          忘记密码
+                        </a>
+                      </div>
+
+                      <div>
+                        <a href="#" data-target="#signup-box" class="user-signup-link">
+                          注册
+                          <i class="ace-icon fa fa-arrow-right"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div><!-- /.widget-body -->
+                </div><!-- /.login-box -->
+
+
+              </div><!-- /.position-relative -->
+
+
+            </div>
+          </div><!-- /.login-container -->
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.main-content -->
+  </div><!-- /.main-container -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+$('body').attr('class', 'login-layout light-login');
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
