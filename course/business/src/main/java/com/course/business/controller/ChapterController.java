a64ff880fadb4954d38444c3157a7a32b1079b8c
hynwg <13488160731@163.com>
2022-04-22 20:05:42 +0800

    增加VO

--- course/business/src/main/java/com/course/business/controller/ChapterController.java
+++ course/business/src/main/java/com/course/business/controller/ChapterController.java
@@ -3,6 +3,8 @@ package com.course.business.controller;
 import com.course.server.entity.Chapter;
 import com.course.server.entity.ChapterExample;
 import com.course.server.service.ChapterService;
+import com.course.server.vo.ChapterVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +12,15 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/chapter")
 public class ChapterController {
     @Resource
     private ChapterService chapterService;
 
-    @RequestMapping("/chapter/select")
-    public List<Chapter> selectByExample(ChapterExample example) {
-        return chapterService.selectByExample(example);
+    @RequestMapping("/select")
+    public List<ChapterVo> selectByExample(ChapterExample example) {
+      return chapterService.selectByExample(example);
+
     }
 
 }
--- course/server/src/main/java/com/course/server/service/ChapterService.java
+++ course/server/src/main/java/com/course/server/service/ChapterService.java
@@ -3,18 +3,29 @@ package com.course.server.service;
 import com.course.server.dao.ChapterMapper;
 import com.course.server.entity.Chapter;
 import com.course.server.entity.ChapterExample;
+import com.course.server.vo.ChapterVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<Chapter> selectByExample(ChapterExample example) {
-        return chapterMapper.selectByExample(example);
-
+    public List<ChapterVo> selectByExample(ChapterExample example) {
+        List<Chapter> chapterList = chapterMapper.selectByExample(example);
+        List<ChapterVo> list = new ArrayList<>();
+        for (int i = 0; i < chapterList.size(); i++) {
+            Chapter chapter = chapterList.get(i);
+            ChapterVo chapterVo = new ChapterVo();
+            BeanUtils.copyProperties(chapter, chapterVo);
+            list.add(chapterVo);
+        }
+        return list;
     }
 }
