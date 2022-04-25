0d59c39d26cb567a80165fe6564acb36295a6930
hynwg <13488160731@163.com>
2022-04-25 21:33:56 +0800

    增加刷新按钮和分页功能

--- course/business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ course/business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,10 +1,11 @@
 package com.course.business.controller.admin;
 
-import com.course.server.entity.Chapter;
-import com.course.server.entity.ChapterExample;
 import com.course.server.service.ChapterService;
 import com.course.server.vo.ChapterVo;
-import org.springframework.beans.BeanUtils;
+import com.course.server.vo.PageVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +18,13 @@ public class ChapterController {
     @Resource
     private ChapterService chapterService;
 
-    @RequestMapping("/select")
-    public List<ChapterVo> selectByExample(ChapterExample example) {
-      return chapterService.selectByExample(example);
+    private static final Logger LOG = LoggerFactory.getLogger(ChapterController.class);
 
+    @RequestMapping("/query")
+    public PageVo query(@RequestBody PageVo pageVo) {
+        LOG.info("pageVo:{}",pageVo);
+        chapterService.query(pageVo);
+        return pageVo;
     }
 
 }
