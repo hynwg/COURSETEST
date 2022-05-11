3912e630f8d5ed73e7e3b1d711689fa2408bf3ed
hynwg <13488160731@163.com>
2022-05-11 19:05:17 +0800

    课程代码优化，隐藏大章和小节管理

--- course/server/src/main/java/com/course/server/service/CourseService.java
+++ course/server/src/main/java/com/course/server/service/CourseService.java
@@ -1,14 +1,17 @@
 package com.course.server.service;
 
+import com.course.server.dao.CourseContentMapper;
 import com.course.server.dao.CourseMapper;
 import com.course.server.entity.Course;
+import com.course.server.entity.CourseContent;
 import com.course.server.entity.CourseExample;
 import com.course.server.util.CopyUtil;
+import com.course.server.vo.CourseContentVo;
 import com.course.server.vo.CoursePageVo;
 import com.course.server.vo.CourseVo;
+import com.course.server.vo.SortVo;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.github.pagehelper.util.StringUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,17 @@ public class CourseService {
     private CourseMapper courseMapper;
     @Resource
     private CourseCategoryService courseCategoryService;
+    @Resource
+    private CourseContentMapper courseContentMapper;
+
+//    @Resource
+//    private TeacherService teacherService;
+
+    @Resource
+    private ChapterService chapterService;
+
+    @Resource
+    private SectionService sectionService;
 
 
     /**
@@ -48,7 +62,7 @@ public class CourseService {
     @Transactional
     public void save(CourseVo courseVo) {
         Course course = CopyUtil.copy(courseVo, Course.class);
-        if (course.getId()==null) {
+        if (course.getId() == null) {
             Date now = new Date();
             course.setCreatedAt(now);
             course.setUpdatedAt(now);
@@ -70,5 +84,49 @@ public class CourseService {
      */
     public void delete(String id) {
         courseMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 查找课程内容
+     */
+    public CourseContentVo findContent(Integer id) {
+        CourseContent content = courseContentMapper.selectByPrimaryKey(id);
+        if (content == null) {
+            return null;
+        }
+        return CopyUtil.copy(content, CourseContentVo.class);
+    }
+
+    /**
+     * 保存课程内容，包含新增和修改
+     */
+    public int saveContent(CourseContentVo courseContentVo) {
+        CourseContent content = CopyUtil.copy(courseContentVo, CourseContent.class);
+        int i = courseContentMapper.updateByPrimaryKeyWithBLOBs(content);
+        if (i == 0) {
+            i = courseContentMapper.insert(content);
+        }
+        return i;
+    }
+
+    /**
+     * 排序
+     *
+     * @param sortDto
+     */
+    @Transactional
+    public void sort(SortVo sortVo) {
+        // 修改当前记录的排序值
+        courseMapper.updateSort(sortVo);
+
+        // 如果排序值变大
+        if (sortVo.getNewSort() > sortVo.getOldSort()) {
+            courseMapper.moveSortsForward(sortVo);
+        }
+
+        // 如果排序值变小
+        if (sortVo.getNewSort() < sortVo.getOldSort()) {
+            courseMapper.moveSortsBackward(sortVo);
+        }
     }
 }
