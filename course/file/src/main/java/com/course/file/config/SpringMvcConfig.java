0dfd6588c1cf6cd866fc74037720f6c09375720b
hynwg <13488160731@163.com>
2022-05-12 14:39:44 +0800

    文件上传,增加Springboot静态资源配置，配置问件访问路径

---
+++ course/file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -0,0 +1,24 @@
+package com.course.file.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 把本地的路径暴露出去，配置静态资源
+ * 访问 http://127.0.0.1:9003/file/f/teacher/xx-头像1.jsp
+ * 相当于 本地 D:/file/imooc/teacher/xx-头像1.jsp
+ *
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Value("${file.path}")
+    private String FILE_PATH;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/f/**").addResourceLocations("file:D:/file/course/" );
+       // registry.addResourceHandler("/f/**").addResourceLocations("file:" + FILE_PATH);
+    }
+}
