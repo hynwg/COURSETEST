f020b63147a83ac806a94a42217ef1b101a4e8a6
hynwg <13488160731@163.com>
2022-05-13 20:28:06 +0800

    跨域问题，解决方法一：在getaway网关的启动类上注入配置，方法二：在项目config目录下见一个CorsConfig.java,实现接口WebMVCConfigurer

--- course/gateway/src/main/java/com/course/gateway/GatewayApplication.java
+++ course/gateway/src/main/java/com/course/gateway/GatewayApplication.java
@@ -24,25 +24,26 @@ public class GatewayApplication {
 		LOG.info("启动成功！！");
 		LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
-//	/**
-//	 * 配置跨域
-//	 * @return
-//	 */
-//	@Bean
-//	public CorsWebFilter corsFilter() {
-//		CorsConfiguration config = new CorsConfiguration();
-//
-//		config.setAllowCredentials(Boolean.TRUE);
-//		config.addAllowedMethod("*");
-//		config.addAllowedOriginPattern("*");
-//		config.addAllowedHeader("*");
-//		config.setMaxAge(3600L);
-//
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
-//		source.registerCorsConfiguration("/**", config);
-//
-//		return new CorsWebFilter(source);
-//	}
+
+	/**
+	 * 配置跨域
+	 * @return
+	 */
+	@Bean
+	public CorsWebFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+
+		config.setAllowCredentials(Boolean.TRUE);
+		config.addAllowedMethod("*");
+		config.addAllowedOriginPattern("*");
+		config.addAllowedHeader("*");
+		config.setMaxAge(3600L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsWebFilter(source);
+	}
 
 
 }
